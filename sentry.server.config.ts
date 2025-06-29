00524244cbeaccd032ceac701a64a262f444fbe4
Ravishek Kumar <ravishekprojects@gmail.com>
2025-06-29 08:21:13 +0530

    updated few files

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://1964c0b74e587189c8fbe41627de0a79@o4506876178464768.ingest.us.sentry.io/4509366354247680",
+  dsn: "https://b62558000cd315400368eefe799a5aad@o4509579545214976.ingest.de.sentry.io/4509579561861200",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
